10260eec35b171e6c9118b8400cc3102bc6cbb09
dmonterroza91 <rudel.mvp1@gmail.com>
2014-06-21 06:49:47 -0600

    pantallas que se me habían olvidado

---
+++ CredCobGer/src/gui/OpcionesMora.java
@@ -0,0 +1,95 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.UIManager;
+import java.awt.Toolkit;
+import java.awt.Font;
+
+public class OpcionesMora extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					OpcionesMora frame = new OpcionesMora();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public OpcionesMora() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(OpcionesMora.class.getResource("/recursos/toolbox.png")));
+		setResizable(false);
+		setTitle("D\u00EDas Mora");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 225, 164);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCuentasCorrientes = new JLabel("Cuentas Corrientes");
+		lblCuentasCorrientes.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblCuentasCorrientes.setBounds(10, 42, 107, 14);
+		contentPane.add(lblCuentasCorrientes);
+		
+		JLabel lblLneasDeCrdito = new JLabel("L\u00EDneas de Cr\u00E9dito");
+		lblLneasDeCrdito.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblLneasDeCrdito.setBounds(10, 17, 107, 14);
+		contentPane.add(lblLneasDeCrdito);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnCancelar.setBounds(124, 98, 89, 23);
+		contentPane.add(btnCancelar);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnAceptar.setBounds(10, 98, 89, 23);
+		contentPane.add(btnAceptar);
+		
+		JLabel lblCuentasAportaciones = new JLabel("Cuentas Aportaciones");
+		lblCuentasAportaciones.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblCuentasAportaciones.setBounds(10, 67, 107, 14);
+		contentPane.add(lblCuentasAportaciones);
+		
+		textField = new JTextField();
+		textField.setBounds(127, 14, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(127, 39, 86, 20);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(127, 64, 86, 20);
+		contentPane.add(textField_2);
+	}
+}
---
+++ CredCobGer/src/gui/Prin_CC.java
@@ -0,0 +1,102 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class Prin_CC extends JFrame {
+
+	private JPanel contentPane;
+	JFrame prin = this;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					Prin_CC frame = new Prin_CC();
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Prin_CC() {
+		
+		setTitle("CredCobGer - Sistema de Informaci\u00F3n Gerencial");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 536, 388);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 528, 21);
+		contentPane.add(menuBar);
+		
+		JMenu mnArchivo = new JMenu("credicob");
+		menuBar.add(mnArchivo);
+		
+		JMenuItem mntmGestinDeCuenta = new JMenuItem("Gesti\u00F3n de Cuenta");
+		mnArchivo.add(mntmGestinDeCuenta);
+		
+		JMenuItem mntmCerrarSesin = new JMenuItem("Cerrar Sesi\u00F3n");
+		mnArchivo.add(mntmCerrarSesin);
+		
+		JMenu mnGestin = new JMenu("Informes");
+		menuBar.add(mnGestin);
+		
+		JMenuItem mntmAbonosACrditos = new JMenuItem("Abonos a Cr\u00E9ditos");
+		mnGestin.add(mntmAbonosACrditos);
+		
+		JMenuItem mntmCrditosPorSucursal = new JMenuItem("Cr\u00E9ditos por Sucursal");
+		mnGestin.add(mntmCrditosPorSucursal);
+		
+		JMenu mnCandidatosARefinanciamiento = new JMenu("Candidatos a Refinanciamiento");
+		mnGestin.add(mnCandidatosARefinanciamiento);
+		
+		JMenuItem mntmBuenAsociado = new JMenuItem("Buen Asociado");
+		mnCandidatosARefinanciamiento.add(mntmBuenAsociado);
+		
+		JMenuItem mntmProblemasEnMora = new JMenuItem("Problemas en Mora");
+		mnCandidatosARefinanciamiento.add(mntmProblemasEnMora);
+		
+		JMenuItem mntmCarteraAsegurable = new JMenuItem("Cartera Asegurable");
+		mnGestin.add(mntmCarteraAsegurable);
+		
+		JMenuItem mntmRecordCrediticio = new JMenuItem("Record Crediticio");
+		mnGestin.add(mntmRecordCrediticio);
+		
+		JMenu mnAdministracin = new JMenu("Administraci\u00F3n");
+		menuBar.add(mnAdministracin);
+		
+		JMenuItem mntmRefrescarBaseDe = new JMenuItem("Refrescar Base de Datos");
+		mnAdministracin.add(mntmRefrescarBaseDe);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(80, 32, 369, 311);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(Prin_CC.class.getResource("/recursos/aclogo_med_trans_369x387.png")));
+		contentPane.add(lblNewLabel);
+	}
+}
