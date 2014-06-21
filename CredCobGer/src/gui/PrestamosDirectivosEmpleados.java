dee518d6546697cd8e02d2b2dd373325343b1520
Cecilia <cecylia239@hotmail.com>
2014-06-21 05:53:11 -0600

    mis tres pantallas

--- CredCobGer/src/gui/PrestamosDirectivosEmpleados.java
+++ CredCobGer/src/gui/PrestamosDirectivosEmpleados.java
@@ -15,6 +15,9 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.Toolkit;
 
 public class PrestamosDirectivosEmpleados extends JFrame {
 
@@ -41,6 +44,8 @@ public class PrestamosDirectivosEmpleados extends JFrame {
 	 * Create the frame.
 	 */
 	public PrestamosDirectivosEmpleados() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(PrestamosDirectivosEmpleados.class.getResource("/recursos/account_balances.png")));
+		setTitle("CredCobGer - Prestamos de Directivos y Empleados");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 620, 364);
 		contentPane = new JPanel();
@@ -49,12 +54,19 @@ public class PrestamosDirectivosEmpleados extends JFrame {
 		contentPane.setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 83, 584, 225);
+		scrollPane.setBounds(10, 99, 584, 209);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
 				{null, null, null, null, null, null, null},
 			},
 			new String[] {
@@ -71,40 +83,55 @@ public class PrestamosDirectivosEmpleados extends JFrame {
 		scrollPane.setViewportView(table);
 		
 		JButton btnGuardar = new JButton("Guardar");
-		btnGuardar.setBounds(10, 27, 89, 23);
+		btnGuardar.setIcon(new ImageIcon(PrestamosDirectivosEmpleados.class.getResource("/recursos/disk.png")));
+		btnGuardar.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		btnGuardar.setBounds(10, 11, 89, 23);
 		contentPane.add(btnGuardar);
 		
 		JLabel lblSucursal = new JLabel("Sucursal");
-		lblSucursal.setBounds(10, 58, 46, 14);
+		lblSucursal.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblSucursal.setBounds(10, 65, 46, 14);
 		contentPane.add(lblSucursal);
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"San Salvador", "Salvador del Mundo"}));
-		comboBox.setBounds(66, 55, 113, 20);
+		comboBox.setBounds(66, 62, 113, 20);
 		contentPane.add(comboBox);
 		
 		JLabel lblDesde = new JLabel("Desde:");
-		lblDesde.setBounds(233, 58, 46, 14);
+		lblDesde.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblDesde.setBounds(218, 65, 46, 14);
 		contentPane.add(lblDesde);
 		
 		JLabel lblHasta = new JLabel("Hasta:");
-		lblHasta.setBounds(368, 58, 46, 14);
+		lblHasta.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblHasta.setBounds(353, 65, 46, 14);
 		contentPane.add(lblHasta);
 		
 		JButton btnFiltrar = new JButton("Filtrar");
-		btnFiltrar.setBounds(503, 54, 89, 23);
+		btnFiltrar.setIcon(new ImageIcon(PrestamosDirectivosEmpleados.class.getResource("/recursos/filter.png")));
+		btnFiltrar.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		btnFiltrar.setBounds(503, 61, 89, 23);
 		contentPane.add(btnFiltrar);
 		
 		JButton btnImprimir = new JButton("Imprimir");
-		btnImprimir.setBounds(114, 27, 89, 23);
+		btnImprimir.setIcon(new ImageIcon(PrestamosDirectivosEmpleados.class.getResource("/recursos/printer.png")));
+		btnImprimir.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		btnImprimir.setBounds(109, 11, 95, 23);
 		contentPane.add(btnImprimir);
 		
 		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(271, 55, 87, 20);
+		dateChooser.setBounds(256, 62, 87, 20);
 		contentPane.add(dateChooser);
 		
 		JDateChooser dateChooser_1 = new JDateChooser();
-		dateChooser_1.setBounds(406, 55, 87, 20);
+		dateChooser_1.setBounds(391, 62, 87, 20);
 		contentPane.add(dateChooser_1);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(PrestamosDirectivosEmpleados.class.getResource("/recursos/aclogo_med_trans_64x67.png")));
+		label.setBounds(533, 0, 61, 58);
+		contentPane.add(label);
 	}
 }
--- CredCobGer/src/gui/PrestamosOtorgadosVRMora.java
+++ CredCobGer/src/gui/PrestamosOtorgadosVRMora.java
@@ -14,6 +14,9 @@ import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import com.toedter.calendar.JDateChooser;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.Toolkit;
 
 public class PrestamosOtorgadosVRMora extends JFrame {
 
@@ -40,18 +43,21 @@ public class PrestamosOtorgadosVRMora extends JFrame {
 	 * Create the frame.
 	 */
 	public PrestamosOtorgadosVRMora() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(PrestamosOtorgadosVRMora.class.getResource("/recursos/account_balances.png")));
+		setTitle("CredCobGer - Prestamos Otorgados VS Mora");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 591, 351);
+		setBounds(100, 100, 613, 351);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 97, 549, 185);
+		scrollPane.setBounds(10, 141, 577, 136);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
+		table.setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 				{null, null, null, null},
@@ -68,54 +74,97 @@ public class PrestamosOtorgadosVRMora extends JFrame {
 		scrollPane.setViewportView(table);
 		
 		JButton btnGuardar = new JButton("Guardar");
+		btnGuardar.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		btnGuardar.setIcon(new ImageIcon(PrestamosOtorgadosVRMora.class.getResource("/recursos/disk.png")));
 		btnGuardar.setBounds(10, 11, 89, 23);
 		contentPane.add(btnGuardar);
 		
 		JButton btnImprimir = new JButton("Imprimir");
-		btnImprimir.setBounds(109, 11, 89, 23);
+		btnImprimir.setIcon(new ImageIcon(PrestamosOtorgadosVRMora.class.getResource("/recursos/printer.png")));
+		btnImprimir.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		btnImprimir.setBounds(109, 11, 102, 23);
 		contentPane.add(btnImprimir);
 		
 		JLabel lblSucursal = new JLabel("Sucursal");
-		lblSucursal.setBounds(10, 45, 46, 14);
+		lblSucursal.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblSucursal.setBounds(10, 57, 46, 14);
 		contentPane.add(lblSucursal);
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Seleccione una...", "San Salvador", "Salvador del Mundo"}));
-		comboBox.setBounds(109, 42, 102, 20);
+		comboBox.setBounds(109, 54, 102, 20);
 		contentPane.add(comboBox);
 		
 		JLabel lblLineaDeCrdito = new JLabel("Linea de Cr\u00E9dito");
-		lblLineaDeCrdito.setBounds(10, 70, 83, 14);
+		lblLineaDeCrdito.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblLineaDeCrdito.setBounds(10, 91, 83, 14);
 		contentPane.add(lblLineaDeCrdito);
 		
 		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Rotativo", "Personal", "Sin Fiador", "Hipotecario", "Navide\u00F1o", "Mercader\u00EDa", "Sin Fiador", "Sobre Aportaciones", "Microcr\u00E9dito"}));
-		comboBox_1.setBounds(110, 67, 101, 20);
+		comboBox_1.setBounds(109, 88, 101, 20);
 		contentPane.add(comboBox_1);
 		
 		JLabel lblDesde = new JLabel("Desde:");
-		lblDesde.setBounds(235, 45, 46, 14);
+		lblDesde.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblDesde.setBounds(245, 91, 46, 14);
 		contentPane.add(lblDesde);
 		
 		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(291, 39, 87, 20);
+		dateChooser.setBounds(301, 85, 87, 20);
 		contentPane.add(dateChooser);
 		
 		JLabel lblHasta = new JLabel("Hasta:");
-		lblHasta.setBounds(386, 45, 46, 14);
+		lblHasta.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblHasta.setBounds(400, 91, 46, 14);
 		contentPane.add(lblHasta);
 		
 		JDateChooser dateChooser_1 = new JDateChooser();
-		dateChooser_1.setBounds(442, 39, 87, 20);
+		dateChooser_1.setBounds(456, 88, 87, 20);
 		contentPane.add(dateChooser_1);
 		
 		JLabel lblCondicion = new JLabel("Condicion");
-		lblCondicion.setBounds(235, 70, 46, 14);
+		lblCondicion.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblCondicion.setBounds(235, 57, 56, 14);
 		contentPane.add(lblCondicion);
 		
 		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
 		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Aprobado", "Pagando Bi\u00E9n", "Con Problemas"}));
-		comboBox_2.setBounds(284, 66, 133, 20);
+		comboBox_2.setBounds(301, 54, 89, 20);
 		contentPane.add(comboBox_2);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(PrestamosOtorgadosVRMora.class.getResource("/recursos/aclogo_med_trans_64x67.png")));
+		label.setBounds(539, 0, 58, 60);
+		contentPane.add(label);
+		
+		JButton btnFiltrar = new JButton("Filtrar");
+		btnFiltrar.setIcon(new ImageIcon(PrestamosOtorgadosVRMora.class.getResource("/recursos/filter.png")));
+		btnFiltrar.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		btnFiltrar.setBounds(454, 53, 89, 23);
+		contentPane.add(btnFiltrar);
+		
+		JLabel lblNewLabel = new JLabel("Totales");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblNewLabel.setBounds(88, 288, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel label_1 = new JLabel("$ 999,999.00");
+		label_1.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		label_1.setBounds(205, 288, 76, 14);
+		contentPane.add(label_1);
+		
+		JLabel label_2 = new JLabel("$ 999,999.00");
+		label_2.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		label_2.setBounds(355, 288, 76, 14);
+		contentPane.add(label_2);
+		
+		JLabel label_3 = new JLabel("$ 999,999.00");
+		label_3.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		label_3.setBounds(494, 288, 76, 14);
+		contentPane.add(label_3);
 	}
 }
