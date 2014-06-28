83d95446942903670841a1cacebb820ba894cca8
dmonterroza91 <rudel.mvp1@gmail.com>
2014-06-28 00:57:16 -0600

    Mapeo de Base de Datos completo...
    Se incluye el archivo de la base de datos en la carpeta DataBase, por
    favor ejecutarlo en su Control Center
    
    No hay explicación del manejo de las clases en hibernate... trabajo en
    ello por el momento

---
+++ CredCobGer/src/main/PruebaDB.java
@@ -0,0 +1,15 @@
+package main;
+
+import credCobGerClasses.TipoUsuario;
+import credCobGerServices.TipoUsuarioService;
+
+public class PruebaDB {
+
+	public static void main(String[] args) {
+
+		
+		
+
+	}
+
+}
